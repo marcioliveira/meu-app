e229fc0c437dc252d6fb83cb955b0f16ef8276f8
marcioliveira <marcioruelaoliveira@gmail.com>
2021-07-18 14:03:21 -0300

    criacao do commponente tipo classe

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-function App() {
-  return <div className="App">hello marcio</div>;
+class App extends React.Component {
+  render() {
+    return (
+      // podemos usar tags html e javascript dentro do nosso método render é o que chamamos de JSX
+      // o simbolo abaixo indica um fragmento que usado para encapusular os elementos que serão retornados
+      <>
+        <p>primeiro paragrafo</p>
+        <p>segundo paragrafo</p>
+        <p>{new Date().toLocaleDateString("pt-BR")}</p>
+      </>
+    );
+  }
 }
 
 export default App;
