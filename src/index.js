dc046e68db80f6a46ed1224b2f556c9204bf5f4b
marcioliveira <marcioruelaoliveira@gmail.com>
2021-07-19 17:56:11 -0300

    ciclo de vida de um componente usando metodo componentdidmount

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App2 from "./components/App2";
 import App3 from "./components/App3";
 import App4 from "./components/App4";
 import App5 from "./components/App5";
+import App6 from "./components/App6";
 import "./style.css";
 import reportWebVitals from "./reportWebVitals";
 
@@ -29,6 +30,7 @@ ReactDOM.render(
     <App3 />
     <App4 />
     <App5 />
+    <App6 />
   </React.StrictMode>,
   document.getElementById("root")
 );
