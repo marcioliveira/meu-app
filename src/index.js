21a340e416d9e1f6365e8942cf7811f2f2a31df5
marcioliveira <marcioruelaoliveira@gmail.com>
2021-07-18 21:21:24 -0300

    passando valores atraves das props e alterando através do state

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./components/App";
+import App2 from "./components/App2";
+import App3 from "./components/App3";
+import App4 from "./components/App4";
+import "./style.css";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
+    {/* <App /> */}
+    <App2
+      title="Titulo usando as props - conteudo dinâmico"
+      text="blablabla usando as props - conteudo dinâmico"
+    >
+      conteudo dinâmico da tag do componente tipo class App2 passado como
+      children
+    </App2>
+    <App2
+      title="Titulo2 usando as props - conteudo dinâmico"
+      text="blablabla usando as props - conteudo dinâmico"
+    >
+      conteudo dinâmico da tag do outro componente tipo class App2 passado como
+      children
+    </App2>
     <App />
+    <App3 />
+    <App4 />
   </React.StrictMode>,
   document.getElementById("root")
 );
